1b9ca2e855cf7c1613977b43cd41602b027ce699
juneyng <120372886+juneyng@users.noreply.github.com>
2023-07-20 11:34:32 +0000

    SS 8 commit

---
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,19 @@
+package hello.core.member;
+
+public class MemberServiceImpl implements MemberService {
+
+    // 가입을 하고 회원을 찾으려면 MemberRepository 인터페이스가 필요하다.
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+    
+}
